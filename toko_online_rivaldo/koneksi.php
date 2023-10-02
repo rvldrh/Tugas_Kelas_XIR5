fa77d45815ec4a5f917d34f44b93ccf5dbf81759
rivaldo <112833805+rvldrh@users.noreply.github.com>
2023-10-02 19:18:12 +0700

    Delete toko_online_rivaldo directory

--- toko_online_rivaldo/koneksi.php
+++
@@ -1,3 +0,0 @@
-<?php 
-    $conn = mysqli_connect("localhost","root","","toko_online_rivaldo");
-?>
--- toko_online_rivaldo/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    session_start();
-    session_destroy();
-    echo "<script>alert('berhasil logout produk');location.href='index.php'</script>";
-?>
